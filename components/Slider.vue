8ca992f7c6e571db3ae7d4f35714e2cf74a7fbc3
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-11-01 18:55:57 +0530

    Slider

---
+++ components/Slider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Slider_row">
+    <div class="row align-items-center">
+      <div class="rounded">
+        <h2 class="text-center mb-5">Lorem, ipsum dolor</h2>
+        <div class="slider">
+          <div class="logos">
+            <i class="fab fa-js fa-4x"></i>
+            <i class="fab fa-linkedin-in fa-4x"></i>
+            <i class="fab fa-dribbble fa-4x"></i>
+            <i class="fab fa-medium-m fa-4x"></i>
+            <i class="fab fa-github fa-4x"></i>
+          </div>
+          <div class="logos">
+            <i class="fab fa-js fa-4x"></i>
+            <i class="fab fa-linkedin-in fa-4x"></i>
+            <i class="fab fa-dribbble fa-4x"></i>
+            <i class="fab fa-medium-m fa-4x"></i>
+            <i class="fab fa-github fa-4x"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
