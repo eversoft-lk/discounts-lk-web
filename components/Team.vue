164d9c207578936fef5c3fc25e6bba35c221eb66
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-11-05 20:56:46 +0530

    Team component

---
+++ components/Team.vue
@@ -0,0 +1,134 @@
+<template>
+  <section class="Team">
+    <div class="container">
+      <div class="row">
+        <div class="text-center mb-md-5" data-aos="fade-up">
+          <h2>Our Team</h2>
+          <p class="mt-3">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6">
+          <div
+            class="d-flex justify-content-center align-items-center flex-column position-relative"
+            data-aos="flip-left"
+          >
+            <figure class="w-100">
+              <img
+                class="w-100"
+                src="/images/About/Team_Member_1.webp"
+                alt="Team_Member"
+              />
+            </figure>
+            <div
+              class="p-lg-4 p-3 position-absolute bottom-0 w-100 text-center d-flex flex-column"
+            >
+              <h4>Peter</h4>
+              <p>Senior Developer</p>
+              <div class="Team_social-icons d-flex justify-content-center">
+                <a href="#" target="_blank"
+                  ><i class="fab fa-facebook-f"></i
+                ></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank"
+                  ><i class="fab fa-linkedin-in"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6">
+          <div
+            class="d-flex justify-content-center align-items-center flex-column position-relative"
+            data-aos="flip-left"
+          >
+            <figure class="w-100">
+              <img
+                class="w-100"
+                src="/images/About/Team_Member_2.webp"
+                alt="Team_Member"
+              />
+            </figure>
+            <div
+              class="p-lg-4 p-3 position-absolute bottom-0 w-100 text-center d-flex flex-column"
+            >
+              <h4>Walker</h4>
+              <p>Senior Business Advisor</p>
+              <div class="Team_social-icons d-flex justify-content-center">
+                <a href="#" target="_blank"
+                  ><i class="fab fa-facebook-f"></i
+                ></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank"
+                  ><i class="fab fa-linkedin-in"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6">
+          <div
+            class="d-flex justify-content-center align-items-center flex-column position-relative"
+            data-aos="flip-left"
+          >
+            <figure class="w-100">
+              <img
+                class="w-100"
+                src="/images/About/Team_Member_3.webp"
+                alt="Team_Member"
+              />
+            </figure>
+            <div
+              class="p-lg-4 p-3 position-absolute bottom-0 w-100 text-center d-flex flex-column"
+            >
+              <h4>Peter</h4>
+              <p>Quality Assurnce</p>
+              <div class="Team_social-icons d-flex justify-content-center">
+                <a href="#" target="_blank"
+                  ><i class="fab fa-facebook-f"></i
+                ></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank"
+                  ><i class="fab fa-linkedin-in"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6">
+          <div
+            class="d-flex justify-content-center align-items-center flex-column position-relative"
+            data-aos="flip-left"
+          >
+            <figure class="w-100">
+              <img
+                class="w-100"
+                src="/images/About/Team_Member_4.webp"
+                alt="Team_Member"
+              />
+            </figure>
+            <div
+              class="p-lg-4 p-3 position-absolute bottom-0 w-100 text-center d-flex flex-column"
+            >
+              <h4>Devako</h4>
+              <p>Designer</p>
+              <div class="Team_social-icons d-flex justify-content-center">
+                <a href="#" target="_blank"
+                  ><i class="fab fa-facebook-f"></i
+                ></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="#" target="_blank"
+                  ><i class="fab fa-linkedin-in"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
